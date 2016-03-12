ddab1eb193cedbf62e64cccfdf146712e5a9959f
Marcus Ilgner <mail@marcusilgner.com>
2016-03-12 22:30:51 +0100

    Improved error handling, commentary

--- server.go
+++ server.go
@@ -3,7 +3,6 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"github.com/dchest/uniuri"
 	"github.com/gin-gonic/gin"
 	"github.com/mediocregopher/radix.v2/pool"
@@ -19,96 +18,103 @@ const (
 
 var logger = logging.MustGetLogger("keedrop")
 
+// structure to store the secret in Redis
+// only the secret key remains with the sender
 type secretData struct {
 	PubKey string `json:"pubkey" binding:"required"`
 	Nonce  string `json:"nonce" binding:"required"`
 	Secret string `json:"secret" binding:"required"`
 }
 
-// stores the secret in Redis
-func saveInRedis(redis *pool.Pool, data *secretData) (string, error) {
+// stores the secret in Redis and returns the key where it can be found
+func saveInRedis(redis *pool.Pool, data *secretData) (string, bool) {
 	conn, err := redis.Get()
 	if err != nil {
-		return "", errors.New("Could not connect to Redis")
+		logger.Error("Could not connect to Redis")
+		return "", false
 	}
 	defer redis.Put(conn)
 
 	jsonData, jsonErr := json.Marshal(data)
 	if jsonErr != nil {
-		return "", errors.New("Could not encode to JSON")
+		logger.Error("Could not marshal secret to JSON.", jsonErr)
+		return "", false
 	}
 	for i := 0; i < maxKeyFindTries; i++ {
 		keyName := uniuri.NewLen(mnemoLen)
 		if _, err := conn.Cmd("SET", keyName, jsonData, "NX", "EX", defaultLifetime).Str(); err == nil {
-			return keyName, nil
+			return keyName, true
+		} else {
+			logger.Error("Could not write secret, probably key collision.", err)
 		}
 	}
-	return "", errors.New("Could not generate key")
+	logger.Error("Could not find key after", maxKeyFindTries, "tries")
+	return "", false
 }
 
 // retrieves the secret from Redis, deleting it at the same time
-func loadFromRedis(redis *pool.Pool, key string) (*secretData, error) {
+func loadFromRedis(redis *pool.Pool, key string) (*secretData, bool) {
 	conn, err := redis.Get()
 	if err != nil {
-		return nil, errors.New("Could not connect to Redis")
+		logger.Error("Could not connect to Redis.", err)
+		return nil, false
 	}
 	defer redis.Put(conn)
-
-	logger.Info("Reading from Redis key ", key)
 
 	conn.PipeAppend("MULTI")
 	conn.PipeAppend("GET", key)
 	conn.PipeAppend("DEL", key)
 	conn.PipeAppend("EXEC")
 
+	// the first 3 commands should only contain "OK" and "QUEUED", no real data
 	for i := 0; i < 3; i++ {
 		if err := conn.PipeResp().Err; err != nil {
-			logger.Error("Redis error:", err)
-			return nil, errors.New("Redis communication failed")
+			logger.Error("Redis error.", err)
+			return nil, false
 		}
 	}
 	if results, err := conn.PipeResp().Array(); err == nil {
+		// array contains the results after MULTI in order
 		encodedData, _ := results[0].Bytes()
-		if len(encodedData) == 0 {
-			return nil, nil
+		if len(encodedData) == 0 { // it means the key wasn't found
+			return nil, true
 		} else {
 			secret := new(secretData)
-			json.Unmarshal(encodedData, secret)
-			return secret, nil
+			if err := json.Unmarshal(encodedData, secret); err == nil {
+				return secret, true
+			} else {
+				logger.Error("Could not unmarshal JSON data: ", encodedData)
+				return nil, false
+			}
 		}
 	} else {
-		logger.Error("Error executing batch:", err)
-		return nil, errors.New("Redis communication failed")
+		logger.Error("Error executing batch.", err)
+		return nil, false
 	}
 }
 
 // the Gin handlers all want a Redis connection, too
 type redisUsingGinHandler func(*pool.Pool, *gin.Context)
 
-// Gin handler to store a secret
+// POST /api/secret
 func storeSecret(redis *pool.Pool, ctx *gin.Context) {
 	var secret secretData
 	if ctx.BindJSON(&secret) == nil {
-		if keyName, err := saveInRedis(redis, &secret); err == nil {
+		if keyName, ok := saveInRedis(redis, &secret); ok {
 			ctx.JSON(http.StatusOK, gin.H{"key": keyName})
 		} else {
-			if err.Error() == "Could not store value" {
-				ctx.JSON(http.StatusConflict, gin.H{"error": "You hit the Jackpot and caused a key collision. Try again."})
-			} else {
-				ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Could not store secret"})
-			}
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Could not store secret"})
 		}
 	} else {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "bad JSON data"})
 	}
 }
 
-// Gin handler to retrieve (and delete) a secret
+// GET /api/secret/:mnemo
 func retrieveSecret(redis *pool.Pool, ctx *gin.Context) {
 	mnemo := ctx.Param("mnemo")
-	logger.Info("Reading data for mnemo:", mnemo)
-	if secret, err := loadFromRedis(redis, mnemo); err != nil {
-		logger.Error("Error reading secret", err)
+	logger.Debug("Reading data for mnemo:", mnemo)
+	if secret, ok := loadFromRedis(redis, mnemo); !ok {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Could not read secret"})
 	} else {
 		if secret == nil {
