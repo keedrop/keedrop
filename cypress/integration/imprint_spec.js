46acd9d19224b73e74ca6c90a0026bb52fdb11d1
Philipp Kursawe <phil.kursawe@gmail.com>
2020-05-23 02:43:05 +0200

    fix: cypress tests

--- cypress/integration/imprint_spec.js
+++ cypress/integration/imprint_spec.js
@@ -5,22 +5,22 @@ context("Imprint page", function () {
       "/imprint"
     ].forEach(url => {
       it("contains the GPG key on " + url, function() {
-        cy.visit(url)
-        cy.contains("E5FF731E").should("exist")
-      })
-    })
-  })
+        cy.visit(url);
+        cy.contains("E5FF731E").should("exist");
+      });
+    });
+  });
 
   describe("region specific informations", function() {
     it("should have required infos on German page", function() {
-      cy.visit('/de/imprint/')
-      cy.contains("Anbieter im Sinne des TDG/MDStV").should("exist")
-    })
+      cy.visit("/de/imprint/");
+      cy.contains("Anbieter im Sinne des TDG/MDStV:").should("exist");
+    });
 
-    it("should have minimal infos on non German page", function() {
-      cy.visit('/imprint/')
-      cy.contains("Anbieter im Sinne des TDG/MDStV").should("not.exist")
-    })
-  })
+    it("should have translated infos on non German page", function() {
+      cy.visit("/imprint/");
+      cy.contains("Contact according with German law:").should("exist");
+    });
+  });
 
-})
+});
--- cypress/integration/noscript_spec.js
+++ cypress/integration/noscript_spec.js
@@ -1,20 +1,18 @@
 describe("noscript", function () {
   it("should show normally when script is enabled", function() {
-    debugger
-    cy.visit("/")
-    cy.contains("The password is encrypted locally and for that you need to enable JavaScript.").should("not.visible")
-    cy.contains("Please enable JavaScript").should("not.visible")
-    cy.get("#encrypt").should("visible").should("be.enabled")
-  })
+    cy.visit("/");
+    cy.contains("The secret is encrypted locally").should("not.visible");
+    cy.contains("Please enable JavaScript").should("not.visible");
+    cy.get("#encrypt").should("visible").should("be.enabled");
+  });
 
   it("should show noscript banner when script is disabled", function() {
-      debugger
-      cy.visit("/", { script: false })
-      cy.contains("The password is encrypted locally and for that you need to enable JavaScript.").should("exist")
-      cy.get('button')
-        .first()
-        .contains("Please enable JavaScript")
-        .should('be.disabled')
-      cy.get("#encrypt").should("not.visible")
-  })
+    cy.visit("/", { script: false });
+    cy.contains("The secret is encrypted locally").should("exist");
+    cy.get("button")
+      .first()
+      .contains("Please enable JavaScript")
+      .should("be.disabled");
+    cy.get("#encrypt").should("not.visible");
+  });
 });
