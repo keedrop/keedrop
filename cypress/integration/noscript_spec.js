ddad85a8d17891b8fd8c0a07f8c8113fa1a883b8
Philipp Kursawe <phil.kursawe@gmail.com>
2020-05-25 00:50:15 +0200

    fix: outdated UI tests

--- cypress/integration/noscript_spec.js
+++ cypress/integration/noscript_spec.js
@@ -1,14 +1,14 @@
 describe("noscript", function () {
   it("should show normally when script is enabled", function() {
     cy.visit("/");
-    cy.contains("The secret is encrypted locally").should("not.visible");
+    cy.contains("Cryptopgrahy is performed in the browser").should("not.visible");
     cy.contains("Please enable JavaScript").should("not.visible");
     cy.get("#encrypt").should("visible").should("be.enabled");
   });
 
   it("should show noscript banner when script is disabled", function() {
     cy.visit("/", { script: false });
-    cy.contains("The secret is encrypted locally").should("exist");
+    cy.contains("Cryptopgrahy is performed in the browser").should("exist");
     cy.get("button")
       .first()
       .contains("Please enable JavaScript")
